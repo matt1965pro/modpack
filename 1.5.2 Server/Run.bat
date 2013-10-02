ddc32d57c78c08d49a67278289a545ee991ed225
matt1965pro <aaron.gentle36@gmail.com>
2013-10-03 07:00:49 +1000

    Updates

--- 1.5.2 Server/Run.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET BINDIR=%~dp0
-CD /D "%BINDIR%"
-"%ProgramFiles%\Java\jre7\bin\java.exe" -Xincgc -Xmx512m -jar mcpc-plus-1.5.2-R1.1-forge738-B644.jar
-PAUSE
--- 1.5.2 Server/readme.txt
+++
@@ -1,3 +0,0 @@
-Server files for 1.5.2
-
-Maintained by matt1965pro

767674a649247a529e37a660027721add51739c8
matt1965pro <aaron.gentle36@gmail.com>
2013-10-02 20:14:32 +1000

    Updates

--- 1.5.2 Server/Run.bat
+++ 1.5.2 Server/Run.bat
@@ -1,5 +1,9 @@
 @ECHO OFF
 SET BINDIR=%~dp0
 CD /D "%BINDIR%"
+<<<<<<< HEAD
 "%ProgramFiles%\Java\jre7\bin\java.exe" -Xincgc -Xmx512m -jar mcpc-plus-1.5.2-R1.1-forge738-B644.jar
+=======
+"%ProgramFiles%\Java\jre7\bin\java.exe" -Xincgc -Xmx6G -jar mcpc-plus-1.5.2-R1.1-forge738-B644.jar
+>>>>>>> Updates
 PAUSE
